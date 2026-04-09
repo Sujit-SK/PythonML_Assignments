55dd0dee1906a3b14661c35e89ac4d7fe23d064f
Sujit-SK <93419442+Sujit-SK@users.noreply.github.com>
2026-04-09 12:50:46 +0530

    Create assignment19_1.py

---
+++ Assignment_19/assignment19_1.py
@@ -0,0 +1,13 @@
+def main():
+    no = int(input("Enter the number: "))
+
+    powerOf = lambda x: x * x
+
+    print("Power of 2 is:", powerOf(no))
+   
+
+    
+   
+
+if __name__ == "__main__":
+    main()
