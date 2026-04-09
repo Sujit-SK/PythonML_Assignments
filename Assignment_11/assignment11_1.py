8ed72e1d235ec6e353a6e0db252d2b4ea89cd757
Sujit-SK <93419442+Sujit-SK@users.noreply.github.com>
2026-04-09 12:39:56 +0530

    Add main function to calculate rectangle area

---
+++ Assignment_11/assignment11_1.py
@@ -0,0 +1,10 @@
+def main():
+    no1 = int(input("Enter the length: "))
+    no2 = int(input("Enter the width: "))
+    
+    area = no1 * no2
+    print("Area of rectangle is :",area)
+    
+    
+if __name__ == "__main__":
+    main()
