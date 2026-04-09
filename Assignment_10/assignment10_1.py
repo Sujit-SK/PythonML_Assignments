66ea36dfa5c3321ef7353be7fb08c642263e2cc5
Sujit-SK <93419442+Sujit-SK@users.noreply.github.com>
2026-04-09 12:38:16 +0530

    Add multiplication table generator in assignment10_1.py

---
+++ Assignment_10/assignment10_1.py
@@ -0,0 +1,11 @@
+def main():
+    no = int(input("Enter the number: "))
+    
+    for i in range(1, 11):
+        result = no * i
+        print(result)
+
+
+
+if __name__ == "__main__":
+    main()
