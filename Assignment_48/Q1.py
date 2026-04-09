3a498ac6e8254fa46bb98ddfa522e9aeb401b69a
Sujit-SK <93419442+Sujit-SK@users.noreply.github.com>
2026-04-09 14:22:00 +0530

    Add script to calculate mean of a dataset

---
+++ Assignment_48/Q1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def main():
+    data = [6, 7, 8, 9, 10, 11, 12]
+
+    mean_data = np.mean(data)
+
+    print("Mean of dataset is : ", mean_data)
+
+if(__name__ == "__main__"):
+    main()
