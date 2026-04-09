066630be3ff9288b8587f45b8b2dc1572ee1dc6f
Sujit-SK <93419442+Sujit-SK@users.noreply.github.com>
2026-04-09 13:59:10 +0530

    Create Q1.py

---
+++ Assignment_3/Q1.py
@@ -0,0 +1,16 @@
+# What is user defined function ?
+# Ans - when we create own function using def key word 
+# that function is called user defined function
+
+# Example :
+
+n1 = 10
+n2 = 20
+
+def multiplication():
+    mult = None
+    mult = n1*n2
+
+    print("Multiplication is :", mult)
+
+multiplication()
